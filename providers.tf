434f334874e07ce9aa3e0f51a4f6d6a5ac7936cf
Shayan Irfan <ishayan767@gmail.com>
2025-01-30 03:51:14 +0500

    feat: cleanup

--- providers.tf
+++ providers.tf
@@ -4,17 +4,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
-    }
   }
 }
 
 provider "aws" {
-  region = "eu-north-1"  # Change this to your desired region
+  region = "eu-north-1"
 }
